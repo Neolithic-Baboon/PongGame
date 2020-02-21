4918c2b59f78a22a599502dce3acd96fcddea12d
Neolithic-Baboon <theraymond1999@gmail.com>
2020-02-21 14:21:11 -0500

    Update main.c

--- main.c
+++ main.c
@@ -20,6 +20,19 @@ char displayChar = 0;
 
 char String[10] = "";
 
+void adc_init() {
+	ADMUX = (1 << REFS0);
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+}
+
+uint16_t adc_read(uint8_t ch) {
+	ch &= 0b00000111;
+	ADMUX = (ADMUX & 0xF8) | ch;
+	ADCSRA |= (1 << ADSC);
+	while(ADCSRA & (1 << ADSC));
+	return(ADC);
+}
+
 int main(void)
 {
 	//setting up the gpio for backlight
@@ -33,6 +46,15 @@ int main(void)
 	PORTB &= ~0x05;
 	PORTB |= 0x00;
 	
+	DDRB |= (1 << PORTB5);
+	PORTB &= ~(1 << PORTB5);
+	
+	DDRC = 0;
+	PORTC = 0;
+	DDRC |= (1 << PORTC1);
+	PORTC &= ~(1 << PORTC1);
+	PORTC |= (1 << PORTB2);
+	
 	//lcd initialisation
 	lcd_init();
 	lcd_command(CMD_DISPLAY_ON);
@@ -41,24 +63,40 @@ int main(void)
 	_delay_ms(10000);
 	clear_buffer(buff);
 	
+	adc_init();
+	
 	while (1)
 	{
-		drawrect(buff, 0, 0, 10, 10);
-		drawcircle(buff, 42, 32, 10);
-		fillrect(buff, 117, 0, 10, 10);
-		fillcircle(buff, 84, 32, 10);
+		
+		uint16_t x_pos, y_pos; 
+		if (!(PINC & (1 << PINC0))) {
+			PORTB |= (1 << PORTB5);
+			DDRC |= (1 << PORTC3);
+			PORTC |= (1 << PORTC0);
+			PORTC &= ~(1 << PORTC0);
+			y_pos = adc_read(0);
+			DDRC &= ~(1 << PORTC1);
+			DDRC &= ~(1 << PORTC3);
+			PORTC &= ~(1 << PORTC3);
+			DDRC |= (1 << PORTC2);
+			PORTC &= ~(1 << PORTC2);
+			DDRC |= (1 << PORTC0);
+			PORTC |= (1 << PORTC0);
+			x_pos = adc_read(3);
+			DDRC = 0;
+			PORTC = 0;
+			DDRC |= (1 << PORTC1);
+			PORTC &= ~(1 << PORTC1);
+			PORTC |= (1 << PORTB2);
+			sprintf(String, "x_pos: %06u y_pos: %06u \n", x_pos, y_pos);
+			USART_putstring(String);
+			
+		} else {
+			PORTB &= ~(1 << PORTB5);
+		}
+		drawstring(buff, 0, 0, "Hello.");
 		write_buffer(buff);
-		_delay_ms(50000);
-		clear_buffer(buff);
-		setpixel(buff, 42, 42);
-		setpixel(buff, 43, 42);
-		setpixel(buff, 44, 42);
-		setpixel(buff, 45, 41);
-		setpixel(buff, 46, 41);
-		setpixel(buff, 47, 40);
-		setpixel(buff, 48, 39);
-		write_buffer(buff);
-		_delay_ms(50000);
+		_delay_ms(500);
 		clear_buffer(buff);
 	}
 }
